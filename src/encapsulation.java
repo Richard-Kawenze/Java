7bf3584c16cdaa922c136b29f89202359751ab92
Richard-Kawenze <richardkawenze22@gmail.com>
2023-06-18 15:34:31 +0300

    continua

---
+++ src/encapsulation.java
@@ -0,0 +1,20 @@
+public class encapsulation {
+    public class person
+    {
+        private String name;
+
+        //Getter
+        public String getName()
+        {
+            return name;
+        }
+    }
+    //Setter
+    public void setName(String newname)
+    {
+        this.name=newname;
+    }
+
+    
+    
+}
