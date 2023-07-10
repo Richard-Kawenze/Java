18ed830955470c1f42172bd79ec448708ee78841
Richard-Kawenze <richardkawenze22@gmail.com>
2023-07-10 11:30:47 +0300

    hashmap

---
+++ src/hashmap.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities=new HashMap<String, String>();
+
+        capitalCities.put("England", "London");
+        capitalCities.put("Kenya","Nairobi");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway","Oslo");
+        System.out.println(capitalCities);
+        
+    }
+
+    
+}
